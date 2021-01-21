8c3b4301b7734f02a313839151e1bd4fdb071b23
Danielryb <daniel.rybaczyk@wp.pl>
2021-01-21 21:54:11 +0100

    dodano funkcję backsubst

--- src/backsubst.c
+++ src/backsubst.c
@@ -8,15 +8,24 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 				/**
 				 * Tutaj należy umieścić właściwą implemntację.
 				 */
-
+				
 				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	if (mat->c == x->r && mat->r == b->r)
+	{
+		int i, l = x->r;
+		for (i =0; i < l; i++) {
+			x->data[l-i-1] = b->data[l-i-1][0];
+			int j;
+			for (j = 0; j < i; j++)
+				x->data[l-i-1][0] -= mat->data[l-i-1][l-j-1] * x->data[l-j-1][0];
+			if(mat->data[l-i-1][l-i-1] != 0)
+				x->data[l-i-1][0] /= mat->data[l-i-1][l-i-1];
+			else
+				return 1;
+		}
+		return 0;
+	}
+	return 2;
 }
 
 
